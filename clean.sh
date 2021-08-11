afd957ae3218c8be9306c2d6161fb16bcfbcee73
Joao Anastacio <joao.anastacio.almeida@ericsson.com>
2021-08-11 12:24:07 +0200

    Updated clean.sh

--- clean.sh
+++ clean.sh
@@ -1,11 +1,11 @@
-echo"removing files from volume"
+echo "removing files from volume"
 docker exec -i web2 /bin/sh << EOF
 cd logFiles/
 ls
 rm *
 ls
 EOF
-echo"stoping containers"
+echo "stoping containers"
 docker stop web1 web2
-echo"removing ghost images"
+echo "removing ghost images"
 docker rmi $(docker images | gawk '{if($1 == "<none>" ) print $3 }')
