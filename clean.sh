8fddb2a43d8239e4a064e2e162e08436b0497e32
Joao Anastacio <joao.anastacio.almeida@ericsson.com>
2021-08-11 10:10:29 +0200

    Updated clean.sh to remove ghost images

--- clean.sh
+++ clean.sh
@@ -1,7 +1,11 @@
+echo"removing files from volume"
 docker exec -i web2 /bin/sh << EOF
 cd logFiles/
 ls
 rm *
 ls
 EOF
+echo"stoping containers"
 docker stop web1 web2
+echo"removing ghost images"
+docker rmi $(docker images | gawk '{if($1 == "<none>" ) print $3 }')
